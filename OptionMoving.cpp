6be403fda7711e28819d04bff2111892d06e6d0f
Remi200 <ponche62880@gmail.com>
2018-01-07 15:17:17 +0100

    add OptionMoving

---
+++ OptionMoving.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#include "OptionMoving.h"
+
+
+//constructeur
+OptionMoving::OptionMoving()
+{
+    m_vitesseBrique = 2 ;
+    m_amplitudeMoving = 200 ;
+    m_directionDroite = true ;
+    m_curseurBrique = 0;
+}
+OptionMoving::~OptionMoving()
+{
+
+}
+
+void OptionMoving::update()
+{
+    if(m_pointeurBrique != 0)
+    {
+        if(m_directionDroite)
+        {
+            m_curseurBrique += m_vitesseBrique;
+            m_pointeurBrique->m_rectangle.move(m_vitesseBrique, 0);
+            if(m_curseurBrique > m_amplitudeMoving)
+                m_directionDroite = false;
+
+        }
+        else
+        {
+            m_curseurBrique -= m_vitesseBrique;
+            m_pointeurBrique->m_rectangle.move(-m_vitesseBrique, 0);
+            if(m_curseurBrique < 0)
+                m_directionDroite = true ;
+        }
+
+    }
+}
+
---
+++ OptionMoving.h
@@ -0,0 +1,27 @@
+#ifndef OPTIONMOVING_H_INCLUDED
+#define OPTIONMOVING_H_INCLUDED
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#include "OptionBrique.h"
+
+class OptionMoving : public OptionBrique
+{
+protected:
+    // attributs
+    int m_vitesseBrique;
+    bool m_directionDroite;
+    int m_amplitudeMoving;
+    int m_curseurBrique;
+
+public:
+    OptionMoving();
+    ~OptionMoving() ;
+
+    void update() ;
+};
+
+
+
+#endif // OPTIONMOVING_H_INCLUDED
